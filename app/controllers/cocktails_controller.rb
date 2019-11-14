1754ddbd0ff8dbc51ddc50653745a3b39bea15c4
Marina Guseva <marina.guseva@contentful.com>
2019-11-14 15:20:09 +0100

    Add cocktails#index&show

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,14 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show; end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+   end
+end
